50aa420528451c063c87fa1c9cd41fa6c14fce38
Phil Philev <phi@mail.bg>
2016-11-21 15:46:49 +0100

    model

---
+++ Model/Project_param.m
@@ -0,0 +1,17 @@
+%Motor parameters
+
+R = 10 ;%Resistance
+L = 130e-6  ;%Inductance 
+K = 11.12e-3 ;%Torque constant
+%Ke = ;%Back emf constant
+J = 2.7e-7 ;%Rotor intertia
+b = 0 ;%Rotor friction
+%Jwheel = ;%Wheel intertia
+%Bwheel = ;%Wheel friction
+
+%Robot parameters
+
+r = 0.065; %wheel radius
+l = 0.12; %distane between wheels
+
+
